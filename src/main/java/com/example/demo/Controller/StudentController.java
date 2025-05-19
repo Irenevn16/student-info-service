d4fe4eb44a933ce27dff6fb2478abe9f36a847db
Irenevn16 <irenevn16@gmail.com>
2025-05-19 17:55:25 +0200

    commit inicial

---
+++ src/main/java/com/example/demo/Controller/StudentController.java
@@ -0,0 +1,21 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public ResponseEntity<?> getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+}
---
+++ src/main/java/com/example/demo/Service/StudentService.java
@@ -0,0 +1,32 @@
+package com.example.demo.Service;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public ResponseEntity<?> getStudentById(Long id) {
+        Optional<Student> student = studentRepository.findById(id);
+
+        if(student.isPresent()) {
+            Student response = student.get();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
